49d911811850706153638b50b1b42849c7e7bbf4
Muhammad Ahsan <ahsanchilll56@gmail.com>
2026-07-25 15:05:12 +0500

    Update identify.js

--- pages/api/identify.js
+++ pages/api/identify.js
@@ -37,7 +37,7 @@ export default async function handler(req, res) {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        model: 'meta-llama/llama-4-scout-17b-16e-instruct',
+        model: 'llama-3.2-90b-vision-preview',
         max_tokens: 1000,
         messages: [
           {
