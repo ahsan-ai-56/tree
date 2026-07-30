72d52ea67605029a32780a899ad224cc81fd59a4
Muhammad Ahsan <ahsanchilll56@gmail.com>
2026-07-30 13:57:38 +0500

    Update maple-tree-identification-guide.js

--- pages/blog/maple-tree-identification-guide.js
+++ pages/blog/maple-tree-identification-guide.js
@@ -244,7 +244,7 @@ export default function MapleTreeIdentificationGuide() {
             {/* IMAGE 1 */}
             <div style={{ margin: '2rem 0', borderRadius: '16px', overflow: 'hidden', border: '1px solid var(--card-border)' }}>
               <img
-                src="mapletree.jpg"
+                src="/mapletree.jpg"
                 alt="Maple tree leaves showing opposite arrangement and lobed shape for identification"
                 style={{ width: '100%', height: '300px', objectFit: 'cover' }}
               />
